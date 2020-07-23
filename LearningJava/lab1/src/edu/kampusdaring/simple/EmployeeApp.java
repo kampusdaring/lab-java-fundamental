c64a16ed8d51bc5f157c67f33d153ddc8a465f6c
Kampus Daring <kamdarindo@gmail.com>
2020-07-23 20:08:59 +0800

    update git ignore

---
+++ LearningJava/lab1/src/edu/kampusdaring/simple/EmployeeApp.java
@@ -0,0 +1,19 @@
+package edu.kampusdaring.simple;
+
+public class EmployeeApp {
+	
+	//inputkan nama pegawai
+	
+	//inputkan umur 
+	
+	//inputkan gaji 
+	
+	//inputkan title pegawai
+	
+	
+	//print nama pegawai, umur, gaji, title pegawai
+	
+	
+	//output : Employee Mr X, 29 years old, Senior Supervisor, 1950000
+	
+}
