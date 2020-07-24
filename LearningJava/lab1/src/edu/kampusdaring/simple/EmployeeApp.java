98a0929f6f0da34093ce2e6590f92e195a83df84
Kampus Daring <kamdarindo@gmail.com>
2020-07-24 16:24:04 +0800

    solution for lab 1

--- LearningJava/lab1/src/edu/kampusdaring/simple/EmployeeApp.java
+++ LearningJava/lab1/src/edu/kampusdaring/simple/EmployeeApp.java
@@ -1,19 +1,35 @@
 package edu.kampusdaring.simple;
 
+import java.util.Scanner;
+
 public class EmployeeApp {
-	
-	//inputkan nama pegawai
-	
-	//inputkan umur 
-	
-	//inputkan gaji 
-	
-	//inputkan title pegawai
-	
-	
-	//print nama pegawai, umur, gaji, title pegawai
-	
-	
-	//output : Employee Mr X, 29 years old, Senior Supervisor, 1950000
-	
+
+	public static void main(String[] args) {
+		// inputkan nama pegawai
+		System.out.println("Please enter employee name : ");
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
+		
+		// inputkan umur
+		System.out.println("Please enter employee age : ");
+		scanner = new Scanner(System.in);
+		int age = scanner.nextInt();
+		
+		// inputkan gaji
+		System.out.println("Please enter employe salary : ");
+		scanner = new Scanner(System.in);
+		double salary = scanner.nextDouble();
+		
+		// inputkan title pegawai
+		System.out.println("Please enter employe role : ");
+		scanner = new Scanner(System.in);
+		String role = scanner.nextLine();
+		
+		// print nama pegawai, umur, gaji, title pegawai
+
+		// output : Employee Mr X, 29 years old, Senior Supervisor, 1950000
+		System.out.println(String.format("%s, %d years old, %s, IDR %s", name, age, role, String.valueOf(salary)));
+		
+	}
+
 }
