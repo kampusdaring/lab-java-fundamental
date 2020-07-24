54626e8ace2f24dba86fc10bfdd2d851922e504b
Kampus Daring <kamdarindo@gmail.com>
2020-07-24 17:30:59 +0800

    condition and while loop

---
+++ LearningJava/lab2/src/edu/kampusdaring/employee/ExamApp.java
@@ -0,0 +1,19 @@
+package edu.kampusdaring.employee;
+
+public class ExamApp {
+
+	public static void main(String[] args) {
+		//login user (input user name)
+		
+		//expected user=bejo
+		
+		//valid user? 
+		
+		//do exam
+		
+		//result = result > 75% ? pass : fail
+		
+		
+	}
+
+}
