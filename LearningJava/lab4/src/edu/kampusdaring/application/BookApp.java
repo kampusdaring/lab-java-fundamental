ed250582bbd6608d651ae35d273e87caecb8b896
Kampus Daring <kamdarindo@gmail.com>
2020-07-31 12:41:38 +0800

    lab 4 : set & map

--- LearningJava/lab4/src/edu/kampusdaring/application/BookApp.java
+++ LearningJava/lab4/src/edu/kampusdaring/application/BookApp.java
@@ -2,7 +2,9 @@ package edu.kampusdaring.application;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import edu.kampusdaring.domain.Book;
@@ -10,6 +12,7 @@ import edu.kampusdaring.domain.Customer;
 
 public class BookApp {
 
+	@SuppressWarnings("unlikely-arg-type")
 	public static void main(String[] args) {
 		//database
 		List<Book> books = getBookList();
@@ -38,6 +41,31 @@ public class BookApp {
 		Customer singleResult = customers.stream().findFirst().filter(customer -> customer.getId().equals(101l)).get();
 		System.out.println(singleResult);
 		
+		
+		//Log O(n) //list
+		
+		//map Log O(1)
+		Map<String, Book> bookMap = getBookMap();
+		
+		System.out.println("Map size : "+ bookMap.size());
+		System.out.println("original : " + bookMap.get("ORACLE_DB"));
+		
+		bookMap.replace("ORACLE_DB", new Book("My SQL BOOK", "my sql db", "kampus daring"));
+		System.out.println("after replace : " + bookMap.get("ORACLE_DB"));
+
+		
+	}
+
+	private static Map<String, Book> getBookMap() {
+		Map<String, Book> bookMap = new HashMap<>();
+		Book javaProgramming = new Book("Java Programming", "Fundamental", "kampus daring");
+		Book webProgramming = new Book("Web Programming", "Essential", "gramedia");
+		Book database = new Book("Oracle DB", "Essential", "oracle");
+		bookMap.put("JAVA", javaProgramming);
+		bookMap.put("WEB", webProgramming);
+		bookMap.put("ORACLE_DB", database);
+		bookMap.put("ORACLE_DB", javaProgramming);
+		return bookMap;
 	}
 
 	private static List<Customer> getCustomers() {
